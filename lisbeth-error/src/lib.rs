1d50806b8ff3232e9d388f1091747d6923ea1177
Sasha Pourcelot <sasha.pourcelot@protonmail.com>
2020-12-17 08:51:28 +0100

    Add initial data structures as well as very simple construction functions.
    This commit adds a bunch of documentation and very little implementation for the
    Span and SpannedStr structures. The difference between these two structures is
    that Span does not hold the content itself. As such, SpannedStr *should* be used
    during parsing while *Span* should be used to represent token position in the
    generated AST.

---
+++ lisbeth-error/src/lib.rs
@@ -0,0 +1,13 @@
+//! Lisbeth-error
+//!
+//! A dead-simple error type for the lisbeth parser infrastructure.
+//!
+//! This type allows provides the `ErrorReport` type, that allows to annotate
+//! input code in order to explain what happened to the user. Annotations are
+//! added to the report by providing a span and an associated message.
+//!
+//! Spans are conveyed thanks to the `SpannedStr` type, which holds the a string
+//! slice and its position in the input stream.
+
+#[forbid(missing_docs)]
+pub mod span;
