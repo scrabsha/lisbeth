4510b8ea7185e856b461e78334b8ac2767a4448e
Sasha Pourcelot <sasha.pourcelot@protonmail.com>
2020-12-24 00:39:20 +0100

    Add lisbeth_tuple_tools, TupleAppend
    For our parser infrastructure, we will rely on simple functions to manipulate tuples. These functions will be declared in the lisbeth_tuple_tools crate.
    
    The first tool implemented in TupleAppend, which allows to append a value of any type at the end of a tuple.

---
+++ lisbeth-tuple-tools/src/append.rs
@@ -0,0 +1,83 @@
+//! Allows to append values at the end of tuples.
+//!
+//! This module provides the `TupleAppend` trait, which allows to append an
+//! element at the end of a tuple.
+//!
+//! # Example
+//!
+//! Here, we append a [`char`] to a `(char, u32)`:
+//!
+//! ```rust
+//! use lisbeth_tuple_tools::TupleAppend;
+//!
+//! let left = ('l', 42).append('s');
+//! let right = ('l', 42, 's');
+//!
+//! assert_eq!(left, right);
+//! ```
+
+/// Allows to append an element at the end of a tuple.
+///
+/// The generic type parameter `T` represents the type to be appended. It may
+/// be removed in the future, when GAT reach stable rust.
+pub trait TupleAppend<T> {
+    /// The type that is returned.
+    type Appended;
+
+    /// The appending function.
+    fn append(self, other: T) -> Self::Appended;
+}
+
+macro_rules! impl_tuple_append {
+    (
+        ( $( $left:ident ),* $(,)? ) + $right:ident $(,)?
+    ) => {
+        impl<$( $left, )* $right> TupleAppend<$right> for ( $( $left, )* ) {
+            type Appended = ( $( $left, )* $right, );
+
+            #[allow(non_snake_case)]
+            fn append(self, other: $right) -> Self::Appended {
+                let ( $( $left, )* ) = self;
+
+                ( $( $left, )* other, )
+            }
+        }
+    };
+}
+
+impl_tuple_append! { () + A }
+impl_tuple_append! { (A,) + B }
+impl_tuple_append! { (A, B) + C }
+impl_tuple_append! { (A, B, C) + D }
+impl_tuple_append! { (A, B, C, D) + E }
+impl_tuple_append! { (A, B, C, D, E) + F }
+impl_tuple_append! { (A, B, C, D, E, F) + G }
+impl_tuple_append! { (A, B, C, D, E, F, G) + H }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn append_empty_tuple() {
+        let t: ((), (), (), (), ()) = ().append(()).append(()).append(()).append(()).append(());
+
+        // Note: there is acutally nothing to be checked, but let's assume so.
+        assert_eq!(t, ((), (), (), (), ()));
+    }
+
+    #[test]
+    fn append_up_to_eight() {
+        let t: (u8, u8, u8, u8, u8, u8, u8, u8) =
+            ().append(1)
+                .append(2)
+                .append(3)
+                .append(4)
+                .append(5)
+                .append(6)
+                .append(7)
+                .append(8);
+
+        assert_eq!(t, (1, 2, 3, 4, 5, 6, 7, 8));
+    }
+}
---
+++ lisbeth-tuple-tools/src/lib.rs
@@ -0,0 +1,13 @@
+//! Some tools to manipulate tuples.
+//!
+//! Most traits in this crate are implemented for tuples with an arity inferior
+//! or equal to eight.
+//!
+//! # `TupleAppend`
+//!
+//! There is no simple way to append a value of type `C` to a tuple of type
+//! `(A, B)` in rust. This is permitted by `TupleAppend`.
+
+mod append;
+
+pub use append::TupleAppend;
